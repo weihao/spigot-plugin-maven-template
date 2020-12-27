b09d08aab7ea22ecd7e0561d49c19fdf0aa7fc71
Weihao <weihao.gao@outlook.com>
2020-12-27 13:56:28 -0500

    Added listener

--- src/main/java/org/akadia/template/Main.java
+++ src/main/java/org/akadia/template/Main.java
@@ -1,16 +1,30 @@
 package org.akadia.template;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getLogger().info("Template plugin is loaded.");
+        Bukkit.getPluginManager().registerEvents(this, this);
         super.onEnable();
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onJoin(PlayerJoinEvent e) {
+        e.getPlayer().sendMessage("Hello world");
+
+        Bukkit.getScheduler().runTaskLater(this, () -> System.out.println("After Lambdas"), 20);
     }
 }
